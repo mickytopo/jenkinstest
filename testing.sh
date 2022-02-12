faa9da6e73a063dbe17251033a5430206934f227
Miguel Toro Aguilar <migueltoroaguilar@MacBook-Air-de-Miguel.local>
2022-02-12 16:34:21 -0400

    ultima prueba de local para ver si funciona

--- testing.sh
+++ testing.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 echo "just some testing push from local mac machine"
-=======
+
 echo "this is the thrid testing just for webhook :) para probar git tree"
->>>>>>> b4eb32dbe951b5b2e120cad85a0b0d792c761180
